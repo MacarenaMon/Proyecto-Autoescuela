12dd5d86fd72571fa048580144408658dc415475
macarenamon <makiimon@hotmail.com>
2022-07-31 22:04:56 -0300

    arreglos

---
+++ stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre: "Practico", tipo: "Practico", desc: "Tu curso ideal para empezar", precio: 1200, img: './img/Multimedia/206.jpg'},
+    {id: 2, nombre: "Teorico", tipo: "Teorico", desc: "Tu curso ideal para empezar", precio: 1100, img: './img/Multimedia/clio.jpg'},
+    {id: 3, nombre: "Estacionamiento", tipo: "Practico", desc: "Tu curso ideal para empezar", precio: 1200, img: './img/Multimedia/fox.jpg'},
+    {id: 4, nombre: "Practico", tipo: "Practico", desc: "Para personas con conocimientos", precio: 1400, img: './img/Multimedia/mobi.webp'},
+    {id: 5, nombre: "Teorico", tipo: "Teorico", desc: "Para personas con conocimientos", precio: 1200, img: './img/Multimedia/fiesta95.jpg'},
+    {id: 6, nombre: "Estacionamiento", tipo: "Practico", desc: "Para personas con conocimientos", precio: 1500, img: './img/Multimedia/goltrend.jpg'},
+]
